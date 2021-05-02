bee49b059b293a3a0b194698090e42bb91505c16
mira04 <mira04@vse.cz>
2021-05-02 14:50:35 +0200

    Added Strava api connection

--- src/components/InputModal.js
+++ src/components/InputModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
 
 import RNPickerSelect from 'react-native-picker-select';
@@ -6,11 +6,53 @@ import RNPickerSelect from 'react-native-picker-select';
 import Modal from 'react-native-modal';
 import Colors from '../constants/Colors';
 import {windowWidth, windowHeight} from '../utils/Dimensions';
+import {authorize} from 'react-native-app-auth';
 
 
 export default function InputModal(props){
 
+    const [tokens, setTokens] = useState();
+
     // console.log(props.conditions);
+    // const config = {
+    //     clientId: '65386',
+    //     clientSecret: '670bdd77878b804b3ff3137e4f7c74c6c3f7e8f7',
+    //     // redirectUrl: 'myapp://oauthredirect',
+    //     redirectUrl: 'org.getchallenged://getchallenged.org/',
+    //     serviceConfiguration: {
+    //       authorizationEndpoint: 'https://www.strava.com/oauth/mobile/authorize',
+    //       tokenEndpoint:
+    //         'https://www.strava.com/oauth/token?client_id=65386&client_secret=670bdd77878b804b3ff3137e4f7c74c6c3f7e8f7',
+    //         // revocationEndpoint: 'https://www.strava.com/oauth/deauthorize'
+
+    //     },
+    //     scopes: ['activity:read_all'],
+    //   };
+      
+    //   async function hangleLogin(){
+    //     try {
+    //         const authState = await authorize(config);
+    //       } catch (e) {
+    //           console.log(e)
+    //       }
+    //   }
+      const openStravaApp = async () => {
+        const config = {
+            clientId: '65386',
+            clientSecret: '670bdd77878b804b3ff3137e4f7c74c6c3f7e8f7',
+            // redirectUrl: 'myapp://oauthredirect',
+            redirectUrl: 'myapp://localhost',
+            // redirectUrl: 'org.getchallenged://getchallenged.org/',
+            serviceConfiguration: {
+              authorizationEndpoint: 'https://www.strava.com/oauth/mobile/authorize',
+              tokenEndpoint: 'https://www.strava.com/oauth/token?client_id=65386&client_secret=670bdd77878b804b3ff3137e4f7c74c6c3f7e8f7',
+    
+            },
+            scopes: ['activity:read_all'],
+        };
+        const result = await authorize(config).then((val) =>console.log(val));
+      };
+
 
     return(
         <Modal isVisible={props.isVisible}
@@ -26,7 +68,7 @@ export default function InputModal(props){
                 <Text>First Modal</Text>
                 <Button  style={{alignSelf:'center'}}
                     title='Connect with Strava'
-                    onPress={()=> handleLogin()}
+                    onPress={()=>openStravaApp()}
                 />
                 
             </View>
